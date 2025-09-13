cf8eba1395672fb2916dbd9315a773f412ec1334
Swetankan Kumar Sinha <157282631+Swetankan@users.noreply.github.com>
2025-09-13 08:16:16 +0530

    Update client.js added redis url, instead of port and host

--- backend/src/redis/client.js
+++ backend/src/redis/client.js
@@ -3,17 +3,16 @@ import Redis from "ioredis";
 
 dotenv.config();
 
-const client = new Redis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: process.env.REDIS_PORT || 6379,
-});
+const redisUrl = process.env.REDIS_URL || "redis://default:GGZGeYBpQXqNlWTXVKFZzPyxSDYtBDSP@redis.railway.internal:6379";
+
+const client = new Redis(redisUrl);
 
 client.on("connect", () => {
-  console.log("✅ Connected to Local Redis (Memurai)");
+console.log("✅ Connected to Redis");
 });
 
 client.on("error", (err) => {
-  console.error("❌ Redis connection error:", err.message);
+console.error("❌ Redis connection error:", err.message);
 });
 
 export default client;
