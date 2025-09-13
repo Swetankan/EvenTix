98c067fed2092c7abfd7499ad895904ad62b54ee
Swetankan Kumar Sinha <157282631+Swetankan@users.noreply.github.com>
2025-09-13 08:30:14 +0530

    Update index.js

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URL}/${DB_NAME}`
+      `${process.env.MONGO_URL}/${DB_NAME}`
     );
     console.log(
       `\nMongoDB connected! DB HOST: ${connectionInstance.connection.host}`
