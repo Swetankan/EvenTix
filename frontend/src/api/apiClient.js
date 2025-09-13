fba25a200e4974c587a5b2aa9c5b5e4b7bd7e2e5
Swetankan Kumar Sinha <157282631+Swetankan@users.noreply.github.com>
2025-09-13 09:41:20 +0530

    Update apiClient.js

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.VITE_API_BASE_URL || "http://localhost:8000/api",
+  baseURL: process.env.VITE_API_BASE_URL,// || "http://localhost:8000/api",
   headers: {
     "Content-Type": "application/json",
   },
